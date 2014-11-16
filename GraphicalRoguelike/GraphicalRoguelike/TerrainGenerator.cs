1c261b46e681d185aaad13216c9c445776ff580e
Samuel Kinnett <samuelkinnett@member.fsf.org>
2014-11-16 20:29:23 +0000

    Improved Terrain Generation
    Algorithm now generates more natural landmasses.

--- GraphicalRoguelike/GraphicalRoguelike/TerrainGenerator.cs
+++ GraphicalRoguelike/GraphicalRoguelike/TerrainGenerator.cs
@@ -17,33 +17,26 @@ namespace GraphicalRoguelike
         {
             int[,] heightMap = new int[worldWidth, worldHeight]; //this array stores the height of each chunk. 0 = bedrock layer, 1 - 8 = ocean, 9 - 31 = land
             int[,] temporaryMap = new int[worldWidth, worldHeight]; //this array is used in the generation process to create the layer before it is applied to the heightmap
-            int[,] cellularMap = new int[worldWidth, worldHeight]; //this array is used in the cellular automata stage of generation;
 
             int neighbourCount = 0; //this is used to store the number of 'alive' tiles surrounding a cell;
 
             Random rand = new Random();
 
             //fileManager.CreateWorldFolder(worldName);
-            double landChance = 0.76;
+            double landChance = 0.70;
 
             //fill the heightmap with bedrock (lowest layer)
-            for (int y = 0; y < worldHeight; y++)
-            {
-                for (int x = 0; x < worldWidth; x++)
-                {
-                    heightMap[x, y] = 0;
-                }
-            }
+            Array.Clear(heightMap, 0, heightMap.Length);
 
             for (int currentHeightLevel = 1; currentHeightLevel < 32; currentHeightLevel++) //for each height level
             {
                 //clear the arrays of their previous contents
                 Array.Clear(temporaryMap, 0, temporaryMap.Length);
 
-                //firstly, fill the available area with random values (0 = empty space, 1 = filled).
-                for (int y = 0; y < worldHeight; y++)
+                //firstly, fill most of the available area with random values (0 = empty space, 1 = filled).
+                for (int y = 2; y < worldHeight - 2; y++)
                 {
-                    for (int x = 0; x < worldWidth; x++)
+                    for (int x = 2; x < worldWidth - 2; x++)
                     {
                         if (rand.NextDouble() < landChance && heightMap[x ,y] == currentHeightLevel - 1)
                         {
@@ -52,8 +45,8 @@ namespace GraphicalRoguelike
                     }
                 }
 
-                //then, simulate a 4-5 rule cellular automata 7 times.
-                //for the first 4 times, fill in areas with less than 2 neighbours
+                //then, simulate a 4-5 rule cellular automata 7 times
+                //for the first 4 times, also fill in areas with 0 neighbours
 
                 for (int generation = 0; generation < 5; generation++)
                 {
@@ -62,66 +55,46 @@ namespace GraphicalRoguelike
                         for (int x = 0; x < worldWidth; x++)
                         {
 
-                            neighbourCount = 0;
+                            neighbourCount = GetNeighbourCount(x, y, worldWidth, worldHeight, temporaryMap);
 
-                            if (y != 0 && y != (worldHeight - 1) && x != 0 && x != (worldWidth - 1)) //stops comparisons being performed on the borders
+                            if (neighbourCount >= 5 || neighbourCount == 0)
                             {
-                                if (temporaryMap[x, y] == 1)
-                                {
-                                    neighbourCount ++;
-                                }
-                                if (temporaryMap[x + 1, y + 1] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x + 1, y] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x + 1, y - 1] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x, y - 1] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x - 1, y - 1] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x - 1, y] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x - 1, y + 1] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                                if (temporaryMap[x, y + 1] == 1)
-                                {
-                                    neighbourCount++;
-                                }
-                            }
-
-                            if (neighbourCount >= 5)
-                            {
-                                cellularMap[x, y] = 1;
+                                temporaryMap[x, y] = 1;
                             }
                             else
                             {
-                                cellularMap[x, y] = 0;
+                                temporaryMap[x, y] = 0;
                             }
 
                         }
                     }
+                }
 
-                    Array.Copy(cellularMap, temporaryMap, cellularMap.Length);
+                //for the last three times, only use the 4-5 rule
 
+                for (int generation = 0; generation < 4; generation++)
+                {
+                    for (int y = 0; y < worldHeight; y++)
+                    {
+                        for (int x = 0; x < worldWidth; x++)
+                        {
+
+                            neighbourCount = GetNeighbourCount(x, y, worldWidth, worldHeight, temporaryMap);
+
+                            if (neighbourCount >= 5)
+                            {
+                                temporaryMap[x, y] = 1;
+                            }
+                            else
+                            {
+                                temporaryMap[x, y] = 0;
+                            }
+
+                        }
+                    }
                 }
 
                 landChance -= 0.01; //make the next level more sparse
-                Array.Clear(cellularMap, 0, cellularMap.Length); //clear the cellular map
 
                 //apply the layer to the heightmap
 
@@ -151,7 +124,245 @@ namespace GraphicalRoguelike
             return chunk;
         }
 
+        private int GetNeighbourCount(int x, int y, int worldWidth, int worldHeight, int[,] temporaryMap)
+        {
+            int neighbourCount = 0;
 
+            if (y != 0 && y != (worldHeight - 1) && x != 0 && x != (worldWidth - 1)) //normal comparisons of non-border tiles
+            {
+                if (temporaryMap[x, y] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x + 1, y + 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x + 1, y] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x + 1, y - 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x, y - 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x - 1, y - 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x - 1, y] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x - 1, y + 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x, y + 1] == 1)
+                {
+                    neighbourCount++;
+                }
+            }
+            else if (y == 0)
+            {
+                if (x == 0) //top left corner
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+                else if (x == worldWidth - 1) //top right corner
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+                else //anywhere along the top border
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+            }
+            else if (x == 0)
+            {
+                if (y == worldHeight - 1) //bottom left corner
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+                else //anywhere along the left border.
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y + 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+            }
+            else if (y == worldHeight - 1)
+            {
+                if (x == worldWidth - 1) //bottom right corner
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+                else //anywhere along the bottom border
+                {
+                    if (temporaryMap[x, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x - 1, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                    if (temporaryMap[x + 1, y - 1] == 1)
+                    {
+                        neighbourCount++;
+                    }
+                }
+            }
+            else if (x == worldWidth - 1) //anywhere along the right border
+            {
+                if (temporaryMap[x, y] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x, y + 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x, y - 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x - 1, y] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x - 1, y + 1] == 1)
+                {
+                    neighbourCount++;
+                }
+                if (temporaryMap[x - 1, y - 1] == 1)
+                {
+                    neighbourCount++;
+                }
+            }
+
+            return neighbourCount;
+        }
 
     }
 }
